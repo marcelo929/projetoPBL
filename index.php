63721dfca0765851689ef6287faf9424d48aaf46
marcelo929 <73357590+marcelo929@users.noreply.github.com>
2021-04-02 21:01:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <title> Projeto PBL </title>
+    <link rel="stylesheet" href="CSS/estilo.css">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+</head>
+
+<body>
+
+<div class="cabeçalho">
+    <h2 class="logo"><i class="fab fa-readme"></i> Projeto PBL</h2>
+  
+    <nav class="menu">
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li>FORMULÁRIO
+                <ul class="subMenu">
+                    <a href="aluno.html">Aluno</a>
+                    <a href="professor.html">Professor</a>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</div>
+    <div class="conteúdo"> 
+        <center>
+        <div id="alerta">
+            <h3 class="clear">Alerta</h3>
+            <br/>
+            <p>É importante que ao ler este artigo, não usar como fonte de pesquisa. O artigo contém dados feitos a partir de uma pesquisa do próprio site, podendo haver valores manipulados da parte dos usuários. Este artigo tem como objetivo preencher a página inicial de um projeto web de escola.</p>
+        </div>
+
+            <br/>
+            <h1>Qual a principal dificuldade dos alunos nas aulas remotas no Brasil e no Maranhão?</h1>
+            <br/>
+            <video id="myvideo">
+                <source src="src/videos/video.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
+                <script>
+                    var video = document.getElementById("myvideo");
+                    if (video.hasAttribute("controls")) {
+                        video.removeAttribute("controls")   
+                    } else {
+                        video.setAttribute("controls","controls")   
+                    }
+                </script>
+            </video>
+    
+            <br/>
+    
+    
+    <p>É certo que com a pandemia do Covid-19 em 2020 o mundo mudou. Foram muitas perdas, seja no campo da economia, saúde, educação ou até mesmo pessoal, com a perda de entes ou debilitação provindas do vírus. Nesta matéria, abordaremos o campo do ensino brasileiro, focando no estado do Maranhão quanto às medidas tomadas para continuar o ensino, mesmo em meio a essa catástrofe.
+        </p>   
+        </center>
+        </div>
+        <div class="coluna2">
+            <center>
+            <br/>
+            <h3>O que é a sala de aula invertida?</h3> 
+            <br/>
+            <p>
+    Antes de tudo, é importante conhecer esse conceito. Também conhecido como <em>flipped classroom</em>, o método consiste em fazer o aluno estudar antes da aula e tirar suas dúvidas durante o momento de sala. É considerado um método eficiente visto que o horário de aula é mais dinâmico e melhor aproveitado.</p>
+    <p>Os professores têm a função de acompanhar o aprendizado do aluno como um tutor, fornecendo material de pesquisa e tirando dúvidas, mas não necessariamente dando aula. Um problema que poucos educadores prestam  atenção nesse sistema é que justamente pelo aluno ser o protagonista, especialmente em um cenário tão atípico, o aprendizado pode tornar-se chato e desinteressante, principalmente durante o ensino fundamental.
+    
+    
+            </p>   
+            </center>
+        </div>
+        <div class="coluna4">
+            <center>
+            <br/>
+            <h3>Como o Maranhão se comportou no início da pandemia?</h3>
+            <br/>
+            <p> O Maranhão entrou em estado de alerta em meados de março de 2020. Nesse período as aulas foram paralisadas e depois de um tempo foi declarado em algumas escolas férias antecipadas de meio de ano, a fim de não criar um crescente atraso no ensino, assim como dar tempo para escolas e universidades não preparadas migrarem para o sistema remoto. Algumas instituições já preparadas migraram rapidamente para o sistema remoto. Muitas escolas particulares adaptaram-se.
+            </p>
+    	<p>O perigo está nas escolas públicas. Demorou demasiadamente para o governo propor medidas e aplicar o ensino remoto em suas instituições. O IFMA (Instituto Federal de Ensino e Tecnologia do Maranhão) campus Monte Castelo, por exemplo, teve as aulas do terceiro ano iniciadas apenas em setembro. Algumas instituições sequer disponibilizaram material para os educadores e alunos terem acesso ao ensino remoto, e outras nem mesmo ensino disponibilizaram.
+    	</p>
+    	<p>Uma metodologia bastante discutida e proposta para o ensino remoto foi a adesão do sistema de sala de aula invertida. Muitas instituições usam tal método.
+            </p>
+            </center>
+        </div>
+</body>
+</html>
